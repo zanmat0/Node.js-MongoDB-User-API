9e5ffab99e697d8561f12265f8483dec7d17d212
Håvard Kjelle <havard.kjelle@ikomm.no>
2024-02-10 05:54:12 +0100

    fix

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     HOST: "**MONGODB-HOST**",
     PORT: 27017,
-    DB: "flagnificent",
+    DB: "**NAME-OF-DB**",
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD
   };
